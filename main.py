31d40875240eef7ef4645407852fc89529e8c505
Hisham Alatni <32318586+desert-wolf20@users.noreply.github.com>
2020-11-18 04:11:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+from licensing.models import *
+from licensing.methods import Key, Helpers
+import os ,socket
+from tkinter import simpledialog
+from datetime import *
     def activateTrial():
-        trial_key = Key.create_trial_key("WyIyMTMzNDEiLCJiN1JuRzRqK3RaOU0yekQxa1hSWTJYN2RsT2VMWDE4UG1Ibk1yQnc5Il0=", 8703, Helpers.GetMachineCode())
+        trial_key = Key.create_trial_key("WyIyMTMzNDEiLCJiN1JuRzK3RaOU0yekQxa1hSWTJYN2RsT2VMWDE4UG1Ibk1yQnc5Il0=", 87, Helpers.GetMachineCode())
 
         if trial_key[0] == None:
             print("An error occurred:dcdc {0}".format(trial_key[1]))
             
         RSAPubKey = "<RSAKeyValue><Modulus>1d1jyuGWHA5f767Iu9EEJN3Uy2D1SgO+5vppD8u9alJggeoJDuDNjL+6o5fMEgfacgD4OweEV+eG6IV5EbFrjJe5L2ay9Hidn6bQUt67hA8z2G112imLnN9h6CkRQFBCC6ew+M73rCSO/Phhfzs/XeYx9YeCv2HSz91HNJG/KDEXbv4bPges5ihIq5nW6Jq5MKyy4F9aN3P/HEQrtsNb+dSslkI3j9aEUufBAMoMgbSo0oUvQ1oQlvqovFqTPqwnEee7LDHUkVgV9BnJJYDw9wQUowlWuyIsokVQVRDtyXKMeBADp9L8KWFTYRELAd+EOFr14eHEDw1oGgkm1e5tkw==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>"
-        auth = "WyIyMTMzNDEiLCJiN1JuRzRqK3RaOU0yekQxa1hSWTJYN2RsT2VMWDE4UG1Ibk1yQnc5Il0="
+        auth = "WyIyMTMzNDEiLCJiN1JuRzK3RaOU0yekQxa1hSWTJYN2RsT2VMWDE4UG1Ibk1yQnc5Il0="
         
         result = Key.activate(token=auth,\
                                  rsa_pub_key=RSAPubKey,\
-                                 product_id=8703, \
+                                 product_id=87, \
                                  key=trial_key[0],\
                                  machine_code=Helpers.GetMachineCode(),\
                                  friendly_name=socket.gethostname())
@@ -69,7 +74,7 @@
                 pubKey = "<RSAKeyValue><Modulus>1d1jyuGWHA5f767Iu9EEJN3Uy2D1SgO+5vppD8u9alJggeoJDuDNjL+6o5fMEgfacgD4OweEV+eG6IV5EbFrjJe5L2ay9Hidn6bQUt67hA8z2G112imLnN9h6CkRQFBCC6ew+M73rCSO/Phhfzs/XeYx9YeCv2HSz91HNJG/KDEXbv4bPges5ihIq5nW6Jq5MKyy4F9aN3P/HEQrtsNb+dSslkI3j9aEUufBAMoMgbSo0oUvQ1oQlvqovFqTPqwnEee7LDHUkVgV9BnJJYDw9wQUowlWuyIsokVQVRDtyXKMeBADp9L8KWFTYRELAd+EOFr14eHEDw1oGgkm1e5tkw==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>"
                 license_key = LicenseKey.load_from_string(pubKey, f.read(),30) ## you can add How many days you want before function return NoneType
         
-            if not Helpers.IsOnRightMachine(license_key):####
+            if not Helpers.IsOnRightMachine(license_key):
                         
                 newactivate()
                 print("NOTE: This license file does not belong to this machine.")
